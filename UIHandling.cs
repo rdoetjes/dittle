da42546d946cf9b881a6018a568257e4b34d13e9
Raymond Doetjes <rdoetjes@phonax.com>
2026-07-09 10:21:05 +0200

    Create UIHandling.cs

---
+++ UIHandling.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using Raylib_cs;
+using System.Numerics;
+
+namespace Dittle
+{
+    public static class UIHandling
+    {
+        public static bool HandleUIInput(ref Board board, ref Player current, ref int depth, ref int? selX, ref int? selY, ref List<Move> moves, ref Move? lastAi, int maxAiDepth)
+        {
+            if (!Raylib.IsMouseButtonPressed(MouseButton.Left)) return false;
+            Vector2 m = Raylib.GetMousePosition();
+
+            // Restart is always allowed
+            if (Raylib.CheckCollisionPointRec(m, new Rectangle(Graphics.BOARD_SIZE_X - 120, 10, 100, 30)))
+            {
+                board = new Board();
+                current = Player.White;
+                selX = null;
+                selY = null;
+                moves.Clear();
+                lastAi = null;
+                return true;
+            }
+
+            // Only allow Level adjustments if no moves have been made yet (Board is in initial state)
+            if (board.WhiteHorizontalMoves == 0 && board.BlackHorizontalMoves == 0 && board.IsInitialBoard())
+            {
+                if (Raylib.CheckCollisionPointRec(m, new Rectangle(210, Graphics.BOARD_SIZE_Y - 80, 40, 40)) && depth > 1)
+                {
+                    depth--;
+                    return true;
+                }
+                if (Raylib.CheckCollisionPointRec(m, new Rectangle(310, Graphics.BOARD_SIZE_Y - 80, 40, 40)) && depth < maxAiDepth)
+                {
+                    depth++;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void HandleBoardInput(Board board, ref Player currentPlayer, ref int? selX, ref int? selY, ref List<Move> moves)
+        {
+            if (!Raylib.IsMouseButtonPressed(MouseButton.Left)) return;
+            Vector2 mouse = Raylib.GetMousePosition();
+            int startX = (Graphics.BOARD_SIZE_X - 7 * Graphics.OFFSET) / 2;
+            int startY = (Graphics.BOARD_SIZE_Y - 7 * Graphics.OFFSET) / 2;
+            int x = (int)((mouse.X - startX) / Graphics.OFFSET);
+            int y = (int)((mouse.Y - startY) / Graphics.OFFSET);
+            if (!board.IsInBounds(x, y)) return;
+
+            if (selX == null)
+            {
+                if (board.Grid[x, y]?.Owner == currentPlayer)
+                {
+                    var allLegalMoves = Rules.GetAllLegalMoves(board, currentPlayer);
+                    var possibleMovesForDie = allLegalMoves.FindAll(m => m.FromX == x && m.FromY == y);
+
+                    // If valid move select that valid move based on current x and y click value.
+                    if (possibleMovesForDie.Count > 0)
+                    {
+                        selX = x;
+                        selY = y;
+                        moves = possibleMovesForDie;
+                    }
+                    // If no moves for this die, we don't select it,
+                    // allowing the user to click another die immediately.
+                }
+            }
+            else
+            {
+                Move move = moves.Find(m => m.ToX == x && m.ToY == y);
+                if (move.FromX == selX && move.FromY == selY)
+                {
+                    AI.ApplyMove(board, move);
+                    currentPlayer = (currentPlayer == Player.White) ? Player.Black : Player.White;
+                }
+                selX = null;
+                selY = null;
+                moves.Clear();
+            }
+        }
+    }
+}
