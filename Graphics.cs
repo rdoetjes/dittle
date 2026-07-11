bd339169ac3b9c027ce987619416300dccfebb33
Raymond Doetjes <rdoetjes@phonax.com>
2026-07-11 06:18:14 +0200

    MOved UIState into it's own file it has no right being in Graphics.cs

--- Graphics.cs
+++ Graphics.cs
@@ -5,24 +5,6 @@ using System.Numerics;
 
 namespace Dittle
 {
-    public struct UiState
-    {
-        public int Depth;
-        public Player CurrentTurn;
-        public Board Board;
-        public int MaxAiDepth;
-        public float MatchTime;
-        public float WhiteThinkTime;
-        public float BlackThinkTime;
-        public int ScoreWhite;
-        public int ScoreBlack;
-        public bool IsAiThinking;
-        public int? SelectedX;
-        public int? SelectedY;
-        public List<Move> LegalMoves;
-        public Move? LastAiMove;
-    }
-
     public static class Graphics
     {
         public const int OFFSET = 60;
